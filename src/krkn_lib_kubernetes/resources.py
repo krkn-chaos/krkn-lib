b743b7396a72065f7c1cbcf60850266a31847876
Tullio Sebastiani <tsebasti@redhat.com>
2023-06-09 18:19:40 +0200

    Rest models + tests

--- src/krkn_lib_kubernetes/resources.py
+++ src/krkn_lib_kubernetes/resources.py
@@ -1,3 +1,6 @@
+import base64
+import yaml
+import json
 from dataclasses import dataclass
 from typing import List
 
@@ -93,3 +96,69 @@ class ChaosResult(LitmusChaosObject):
 
 class ApiRequestException(Exception):
     pass
+
+
+@dataclass(order=False)
+class ScenarioTelemetry:
+    startTimeStamp: float
+    endTimeStamp: float
+    scenario: str
+    exitStatus: int
+    parametersBase64: str
+    parameters: any
+
+    def __init__(self, json_object: any = None):
+        if json_object is not None:
+            self.startTimeStamp = int(json_object.get("startTimeStamp"))
+            self.endTimeStamp = json_object.get("endTimeStamp")
+            self.scenario = json_object.get("scenario")
+            self.exitStatus = json_object.get("exitStatus")
+            self.parametersBase64 = json_object.get("parametersBase64")
+            self.parameters = json_object.get("parameters")
+
+            if (
+                self.parametersBase64 is not None
+                and self.parametersBase64 != ""
+            ):
+                try:
+                    yaml_params = base64.b64decode(self.parametersBase64)
+                    yaml_object = yaml.safe_load(yaml_params)
+                    # if is not a dictionary means that the base64
+                    # encoded string is not a structured object
+                    if isinstance(yaml_object, dict) is False:
+                        raise Exception("parameters are in the wrong format")
+
+                    json_string = json.dumps(yaml_object, indent=2)
+                    self.parameters = json.loads(json_string)
+                    self.parametersBase64 = ""
+                except Exception as e:
+                    raise Exception(
+                        "invalid parameters format: {0}".format(str(e))
+                    )
+        else:
+            # if constructor is called without params
+            # property are initialized so are available
+            self.startTimeStamp = 0
+            self.endTimeStamp = 0
+            self.scenario = ""
+            self.exitStatus = 0
+            self.parametersBase64 = ""
+            self.parameters = {}
+
+
+@dataclass(order=False)
+class ChaosRunTelemetry:
+    scenarios: list[ScenarioTelemetry]
+
+    def __init__(self, json_object: any = None):
+        self.scenarios = []
+        if json_object is not None:
+            scenarios = json_object.get("scenarios")
+            if scenarios is None or isinstance(scenarios, list) is False:
+                raise Exception("scenarios param must be a list of object")
+            for scenario in scenarios:
+                scenario_telemetry = ScenarioTelemetry(scenario)
+                self.scenarios.append(scenario_telemetry)
+
+    def to_json(self) -> str:
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
