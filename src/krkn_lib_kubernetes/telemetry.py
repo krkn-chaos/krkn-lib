f543a2a8f9130c9792e475b1ea2996bccf979798
Tullio Sebastiani <tsebasti@redhat.com>
2023-06-09 18:18:56 +0200

    Telemetry class + tests

---
+++ src/krkn_lib_kubernetes/telemetry.py
@@ -0,0 +1,99 @@
+import base64
+import logging
+import yaml
+import requests
+import os
+from krkn_lib_kubernetes import ChaosRunTelemetry, ScenarioTelemetry
+
+
+class KrknTelemetry:
+    def send_telemetry(
+        self,
+        telemetry_config: dict,
+        chaos_telemetry: ChaosRunTelemetry,
+    ):
+        """
+
+        :param chaos_telemetry: already populated Chaos
+        :param telemetry_config: krkn telemetry conf section
+        :param scenario_input_file: path to the scenario input yaml
+        :return:
+        """
+        enabled = telemetry_config.get("enabled")
+        if enabled:
+            url = telemetry_config.get("api_url")
+            username = telemetry_config.get("username")
+            password = telemetry_config.get("password")
+            exceptions = []
+            is_exception = False
+            if url is None:
+                exceptions.append("telemetry url is None")
+                is_exception = True
+            if username is None:
+                exceptions.append("telemetry url is None")
+                is_exception = True
+            if password is None:
+                exceptions.append("telemetry password is none")
+                is_exception = True
+            if is_exception:
+                raise Exception(", ".join(exceptions))
+
+            # load config file
+            headers = {
+                "Content-type": "application/json",
+                "Accept": "text/plain",
+            }
+            request = requests.post(
+                url=url,
+                auth=(username, password),
+                data=chaos_telemetry.to_json(),
+                headers=headers,
+            )
+
+            if request.status_code != 200:
+                logging.warning("failed to send telemetry with error: {0}")
+
+    def set_parameters_base64(
+        self, scenario_telemetry: ScenarioTelemetry, file_path: str
+    ):
+        input_file_data = ""
+        input_file_yaml = None
+        if not os.path.exists(file_path):
+            raise Exception(
+                "telemetry : scenario file not found {0} ".format(file_path)
+            )
+
+        with open(file_path, "rb") as file_stream:
+            input_file_data = file_stream.read().decode("utf-8")
+            if input_file_data is None:
+                raise Exception(
+                    "telemetry : empty scenario file {0} ".format(file_path)
+                )
+        try:
+            input_file_yaml = yaml.safe_load(input_file_data)
+            # anonymize kubeconfig option in input
+            self.deep_set_attribute(
+                "kubeconfig", "anonymized", input_file_yaml
+            )
+            input_file_data = yaml.safe_dump(input_file_yaml)
+            input_file_base64 = base64.b64encode(
+                input_file_data.encode()
+            ).decode()
+        except Exception as e:
+            raise Exception("telemetry: {0}".format(str(e)))
+        scenario_telemetry.parametersBase64 = input_file_base64
+
+    def deep_set_attribute(self, attribute: str, value: str, obj: any) -> any:
+        if isinstance(obj, list):
+            for element in obj:
+                self.deep_set_attribute(attribute, value, element)
+        if isinstance(obj, dict):
+            for key in obj.keys():
+                if isinstance(obj[key], dict):
+                    self.deep_set_attribute(attribute, value, obj[key])
+                elif isinstance(obj[key], list):
+                    for element in obj[key]:
+                        self.deep_set_attribute(attribute, value, element)
+                if key == attribute:
+                    obj[key] = value
+        return obj
---
+++ src/krkn_lib_kubernetes/test_telemetry.py
@@ -0,0 +1,83 @@
+import base64
+import unittest
+import yaml
+from krkn_lib_kubernetes import KrknTelemetry, ScenarioTelemetry
+
+
+class KrknTelemetryTests(unittest.TestCase):
+    lib_telemetry: KrknTelemetry
+
+    @classmethod
+    def setUpClass(cls):
+        cls.lib_telemetry = KrknTelemetry()
+
+    def test_deep_set_attribute(self):
+        deep_yaml = """
+            test:
+                - element: __MARKER__
+                  property_1: test
+                  property_2: test
+                  obj_1:
+                    element: __MARKER__
+                    obj_1:
+                        element: __MARKER__
+                        property_1: test
+                        property_2: 
+                            - property_3: test
+                              property_4: test
+                            - property_5:
+                                element: __MARKER__
+            """
+
+        deep_obj = yaml.safe_load(deep_yaml)
+        self.lib_telemetry.deep_set_attribute(
+            "element", "__UPDATED__", deep_obj
+        )
+
+        unserialized_updated_object = yaml.safe_dump(deep_obj, indent=4)
+        self.assertEqual(unserialized_updated_object.count("__UPDATED__"), 4)
+        self.assertEqual(unserialized_updated_object.count("__MARKER__"), 0)
+
+    def test_set_parameters_base_64(self):
+        file_path = "src/testdata/input.yaml"
+        scenario_telemetry = ScenarioTelemetry()
+        telemetry = KrknTelemetry()
+        telemetry.set_parameters_base64(scenario_telemetry, file_path)
+        with open(file_path, "rb") as file_stream:
+            input_file_data_orig = file_stream.read().decode("utf-8")
+            self.assertIsNotNone(input_file_data_orig)
+
+        input_file_yaml_orig = yaml.safe_load(input_file_data_orig)
+        input_file_yaml_processed = yaml.safe_load(
+            base64.b64decode(
+                scenario_telemetry.parametersBase64.encode()
+            ).decode()
+        )
+
+        # once deserialized the base64 encoded parameter must be
+        # equal to the original file except for the attribut kubeconfig
+        # that has been set to "anonymized"
+        self.assertEqual(
+            len(input_file_yaml_processed["input_list"]),
+            len(input_file_yaml_orig["input_list"]),
+        )
+
+        for key in input_file_yaml_orig["input_list"][0].keys():
+            if key != "kubeconfig":
+                self.assertEqual(
+                    input_file_yaml_orig["input_list"][0][key],
+                    input_file_yaml_processed["input_list"][0][key],
+                )
+            else:
+                self.assertNotEquals(
+                    input_file_yaml_orig["input_list"][0][key],
+                    input_file_yaml_processed["input_list"][0][key],
+                )
+                self.assertEqual(
+                    input_file_yaml_processed["input_list"][0][key],
+                    "anonymized",
+                )
+
+
+if __name__ == "__main__":
+    unittest.main()
