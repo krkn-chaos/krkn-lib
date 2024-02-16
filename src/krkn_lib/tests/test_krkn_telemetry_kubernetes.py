22b43176ec1716aad01dd31b4bb8823cdbcf47cc
Tullio Sebastiani <tsebastiani@users.noreply.github.com>
2024-02-16 16:08:17 +0100

    updated tests with telemetry API dev endpoint
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    run tests
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- src/krkn_lib/tests/test_krkn_telemetry_kubernetes.py
+++ src/krkn_lib/tests/test_krkn_telemetry_kubernetes.py
@@ -162,7 +162,7 @@ class KrknTelemetryKubernetesTests(BaseTest):
             "username": os.getenv("API_USER"),
             "password": os.getenv("API_PASSWORD"),
             "max_retries": 5,
-            "api_url": "https://ulnmf9xv7j.execute-api.us-west-2.amazonaws.com/production",  # NOQA
+            "api_url": "https://9ead3157ti.execute-api.us-west-2.amazonaws.com/dev",  # NOQA
             "backup_threads": 6,
             "archive_path": test_workdir,
             "prometheus_backup": "True",
@@ -213,7 +213,7 @@ class KrknTelemetryKubernetesTests(BaseTest):
             "username": os.getenv("API_USER"),
             "password": os.getenv("API_PASSWORD"),
             "max_retries": 5,
-            "api_url": "https://ulnmf9xv7j.execute-api.us-west-2.amazonaws.com/production",  # NOQA
+            "api_url": "https://9ead3157ti.execute-api.us-west-2.amazonaws.com/dev",  # NOQA
             "backup_threads": 6,
             "archive_path": request_id,
             "prometheus_backup": "True",
@@ -246,7 +246,7 @@ class KrknTelemetryKubernetesTests(BaseTest):
             "username": os.getenv("API_USER"),
             "password": os.getenv("API_PASSWORD"),
             "max_retries": 5,
-            "api_url": "https://ulnmf9xv7j.execute-api.us-west-2.amazonaws.com/production",  # NOQA
+            "api_url": "https://9ead3157ti.execute-api.us-west-2.amazonaws.com/dev",  # NOQA
             "backup_threads": 6,
         }
         now = datetime.datetime.now()
@@ -276,7 +276,7 @@ class KrknTelemetryKubernetesTests(BaseTest):
             "username": os.getenv("API_USER"),
             "password": os.getenv("API_PASSWORD"),
             "max_retries": 5,
-            "api_url": "https://ulnmf9xv7j.execute-api.us-west-2.amazonaws.com/production",  # NOQA
+            "api_url": "https://9ead3157ti.execute-api.us-west-2.amazonaws.com/dev",  # NOQA
             "backup_threads": 6,
             "archive_path": test_workdir,
             "prometheus_backup": "True",
