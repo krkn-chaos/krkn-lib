db785bcdffbe73bafa55c3b0fea2e7fc0ecc360b
Tullio Sebastiani <tsebastiani@users.noreply.github.com>
2024-02-28 17:04:30 +0100

    Telemetry ws rework to add telemetry group support (#99)
    * added bucketobject rest model
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    added last modified
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    * added group to telemetry files upload
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    linting
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    * fixed test to match the new API
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>
    
    ---------
    
    Signed-off-by: Tullio Sebastiani <tsebasti@redhat.com>

--- src/krkn_lib/telemetry/ocp/krkn_telemetry_openshift.py
+++ src/krkn_lib/telemetry/ocp/krkn_telemetry_openshift.py
@@ -82,6 +82,7 @@ class KrknTelemetryOpenshift(KrknTelemetryKubernetes):
         archive_path = telemetry_config.get("archive_path")
         logs_filter_patterns = telemetry_config.get("logs_filter_patterns")
         oc_cli_path = telemetry_config.get("oc_cli_path")
+        group = telemetry_config.get("telemetry_group")
         exceptions = []
         is_exception = False
         if logs_backup is None:
@@ -114,6 +115,8 @@ class KrknTelemetryOpenshift(KrknTelemetryKubernetes):
         if logs_filter_patterns is None:
             exceptions.append("telemetry -> logs_filter_pastterns is missing")
             is_exception = True
+        if not group:
+            group = self.default_telemetry_group
 
         # if oc_cli_path is set to empty will be set to
         # None to let the config flexible
@@ -171,6 +174,7 @@ class KrknTelemetryOpenshift(KrknTelemetryKubernetes):
                 queue,
                 queue_size,
                 request_id,
+                group,
                 f"{url}/presigned-url",
                 username,
                 password,
