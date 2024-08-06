6efebd34c48baa0d315e47a7a985e137fc793ae3
Auto User <auto@users.noreply.github.com>
2024-08-06 16:23:11 -0400

    updating tests
    Signed-off-by: Auto User <auto@users.noreply.github.com>

--- src/krkn_lib/tests/test_krkn_elastic.py
+++ src/krkn_lib/tests/test_krkn_elastic.py
@@ -10,15 +10,21 @@ class TestKrknElastic(BaseTest):
     url = os.getenv("ES_SERVER")
     safe_logger: SafeLogger = SafeLogger()
 
-    def _testupload_correct(self):
+    def testupload_correct(self):
         elastic = KrknElastic(self.safe_logger, self.url)
         time = elastic.upload_data_to_elasticsearch(
-            {"timestamp": datetime.datetime.now()}, "chaos_test"
+            {
+                "timestamp": datetime.datetime.now(),
+                "run_uuid": "03264e35-126e-4027-b49e-96bd878a53ac",
+            },
+            "chaos_test",
         )
+
+        print("example data" + str())
 
         self.assertGreater(time, 0)
 
-    def _testupload_no_index(self):
+    def testupload_no_index(self):
         elastic = KrknElastic(self.safe_logger, self.url)
         time = elastic.upload_data_to_elasticsearch(
             {"timestamp": datetime.datetime.now()}, ""
@@ -26,19 +32,16 @@ class TestKrknElastic(BaseTest):
 
         self.assertEqual(time, 0)
 
-    def _testupload_bad_es_url(self):
+    def testupload_bad_es_url(self):
         elastic = KrknElastic(self.safe_logger, "https://localhost")
         time = elastic.upload_data_to_elasticsearch(
-            {"timestamp": datetime.datetime.now()}, "chaos_test"
+            {"timestamp": datetime.datetime.now()}, ""
         )
-
-        self.assertEqual(time, -1)
-
-    def _testupload_blank_es_url(self):
-        es_url = ""
-        elastic = KrknElastic(self.safe_logger, es_url)
-        time = elastic.upload_data_to_elasticsearch(
-            {"timestamp": datetime.datetime.now()}, "chaos_test"
-        )
-
         self.assertEqual(time, 0)
+
+    def testupload_blank_es_url(self):
+        try:
+            es_url = ""
+            KrknElastic(self.safe_logger, es_url)
+        except Exception as e:
+            self.assertEqual(str(e), "elastic search url is not valid")
