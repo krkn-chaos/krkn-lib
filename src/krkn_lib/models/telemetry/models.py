391badc9b601798d5da9f4e6b2c63c6c4b239fc6
Auto User <auto@users.noreply.github.com>
2024-08-08 08:42:13 -0400

    adding version"

--- src/krkn_lib/models/telemetry/models.py
+++ src/krkn_lib/models/telemetry/models.py
@@ -274,17 +274,18 @@ class ChaosRunTelemetry:
             scenarios = json_dict.get("scenarios")
             if scenarios is None or isinstance(scenarios, list) is False:
                 raise Exception("scenarios param must be a list of object")
-            for scenario in scenarios:
-                scenario_telemetry = ScenarioTelemetry(scenario)
-                self.scenarios.append(scenario_telemetry)
 
-            self.node_summary_infos = json_object.get("node_summary_infos")
-            self.node_taints = json_object.get("node_taints")
-            self.total_node_count = json_object.get("total_node_count")
-            self.cloud_infrastructure = json_object.get("cloud_infrastructure")
-            self.cloud_type = json_object.get("cloud_type")
-            self.version = json_object.get("version")
-            self.kubernetes_objects_count = json_object.get(
+            self.scenarios = [ScenarioTelemetry(s) for s in scenarios]
+
+            self.node_summary_infos = [
+                NodeInfo(j) for j in json_dict.get("node_summary_infos")
+            ]
+            self.node_taints = [Taint(t) for t in json_dict.get("node_taints")]
+            self.total_node_count = json_dict.get("total_node_count")
+            self.cloud_infrastructure = json_dict.get("cloud_infrastructure")
+            self.cloud_type = json_dict.get("cloud_type")
+            self.version = json_dict.get("version")
+            self.kubernetes_objects_count = json_dict.get(
                 "kubernetes_objects_count"
             )
             self.network_plugins = json_dict.get("network_plugins")
