3d7170672dba8991df13631fbe791585d1083493
kattameghana <meghanakatta8@gmail.com>
2025-03-09 22:21:29 +0530

    elastic changes
    Signed-off-by: kattameghana <meghanakatta8@gmail.com>

--- src/krkn_lib/models/telemetry/models.py
+++ src/krkn_lib/models/telemetry/models.py
@@ -504,7 +504,7 @@ class ChaosRunTelemetry:
             self.network_plugins = json_dict.get("network_plugins")
             self.run_uuid = json_dict.get("run_uuid")
             self.timestamp = json_dict.get("timestamp")
-            self.health_checks = [HealthCheck(k) for k in json_dict.get("health_checks")] if json_dict.get("health_checks")  else None
+            self.health_checks = [HealthCheck(k) for k in json_dict.get("health_checks")] if json_dict.get("health_checks") else None
     def to_json(self) -> str:
         return json.dumps(self, default=lambda o: o.__dict__, indent=4)
 
